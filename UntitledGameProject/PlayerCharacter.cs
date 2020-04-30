105e197a2c405f205cf8397e18a1dc919e62184b
dtimmins <dantimmins97@gmail.com>
2020-04-29 20:31:33 -0400

    Added basic character, the Dragon Maw in, and the first quest.

---
+++ UntitledGameProject/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UntitledGameProject
+{
+    class PlayerCharacter
+    {
+        public string CharacterName { get; set; }
+        public string Weapon { get; set; }
+        public PlayerCharacter(string characterName, string weapon)
+        {
+            CharacterName = characterName;
+            Weapon = weapon;
+        }
+        
+    }
+}
